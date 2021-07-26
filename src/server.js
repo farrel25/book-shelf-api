97b26e66af6626e8d38bbfb6a07786704492fdc6
Farrel Putra <63508979+farrel25@users.noreply.github.com>
2021-07-26 07:01:26 +0700

    set up project and create server
    - install some modules
    - create server using hapi
    - create modular js files
    - implement standardjs code style using eslint

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-console.log('eh kok jalan');
+const Hapi = require('@hapi/hapi')
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost'
+  })
+
+  await server.start()
+  console.log(`Server running on ${server.info.uri}`)
+}
+
+process.on('unhandledRejection', (err) => {
+  console.log(err)
+  process.exit(1)
+})
+
+init()
