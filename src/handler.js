58f18f863e9848002665803ad25c0ff4f4c43c0a
Farrel Putra <63508979+farrel25@users.noreply.github.com>
2021-07-26 09:40:32 +0700

    edit book by id handler, done

--- src/handler.js
+++ src/handler.js
@@ -80,4 +80,53 @@ const getBookByIdHandler = (request, h) => {
   return response
 }
 
-module.exports = { addBookHanlder, getAllBooksHandler, getBookByIdHandler }
+const editBookByIdHandler = (request, h) => {
+  const { bookId } = request.params
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload
+  const updatedAt = new Date().toISOString()
+  const bookIndex = bookShelf.findIndex(book => book.id === bookId)
+
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku'
+    })
+    response.code(400)
+    return response
+  } else if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount'
+    })
+    response.code(400)
+    return response
+  } else if (bookIndex !== -1) {
+    bookShelf[bookIndex] = {
+      ...bookShelf[bookIndex],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      reading,
+      updatedAt
+    }
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui'
+    })
+    response.code(200)
+    return response
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui buku. Id tidak ditemukan'
+  })
+  response.code(404)
+  return response
+}
+
+module.exports = { addBookHanlder, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler }
--- src/routes.js
+++ src/routes.js
@@ -15,6 +15,11 @@ const routes = [
     method: 'GET',
     path: '/books/{bookId}',
     handler: Handler.getBookByIdHandler
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: Handler.editBookByIdHandler
   }
 ]
 
